8b34272ddd0c332071aadf9614f12eb2c0071c51
hpuliyedath <hpuliyedath@vlocity.com>
2017-12-08 13:30:44 -0800

    performance enhancement emacs project setup

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (project-root . "/home/puhari2001/my_learning/web_performance/web-workers-demo")))
